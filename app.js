65918e51d5680997e95852ff7883da5a794fff14
Nero <nerochen25@berkeley.edu>
2019-04-02 00:24:47 -0700

    Add routes for users and courts

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const courts = require("./routes/api/courts");
+const bodyParser = require('body-parser');
+
 
 app.get("/", (req, res) => res.send("Hello World, Dante"));
-
+app.use("/api/users", users);
+app.use("/api/courts", courts);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 mongoose
