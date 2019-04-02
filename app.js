dfc22650620f67fc587141fdf095934fd6b0c78a
Nero <nerochen25@berkeley.edu>
2019-04-02 01:00:41 -0700

    Enable user registration route and successfully register with username, email and password

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const users = require("./routes/api/users");
 const courts = require("./routes/api/courts");
 const bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World, Dante"));
 app.use("/api/users", users);
 app.use("/api/courts", courts);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 mongoose
