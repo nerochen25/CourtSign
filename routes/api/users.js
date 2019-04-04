a1e73990f37c8c6b8dba042379030c4078e9372a
Nero <nerochen25@berkeley.edu>
2019-04-03 21:04:41 -0700

    Set up login route and jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users router" }));
@@ -33,5 +35,38 @@ router.post('/register', (req, res) => {
         }
       })
   })
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, name: user.username};
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              }
+            );
+          } else {
+            return res.status(404).json({password: 'Incorrect password'});
+          }
+        })
+    })
+})
 
 module.exports = router;
